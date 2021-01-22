db17ba5d2d89751ec69324f644e796d5c64f986f
OperationXen <OperationXen@apocryphasecurity.com>
2021-01-22 09:07:31 +0000

    Moved covenant abilities to dedicated file

---
+++ cooldowns_covenants.lua
@@ -0,0 +1,25 @@
+-- Covenants/Major spells
+
+-- Purify Soul, effect of Phial of Serenity (do not track Summon Steward)
+LCT_SpellData[323436] = {
+  covenant = "KYRIAN",
+  cooldown = 180
+}
+
+-- Door of Shadows
+LCT_SpellData[300728] = {
+  covenant = "VENTHYR",
+  cooldown = 60
+}
+
+-- Fleshcraft
+LCT_SpellData[324631] = {
+  covenant = "NECROLORD",
+  cooldown = 120
+}
+
+-- Soulshape
+LCT_SpellData[310143] = {
+  covenant = "NIGHTFAE",
+  cooldown = 90,
+}
